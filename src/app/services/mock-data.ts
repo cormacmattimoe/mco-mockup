9f56538bf89ba02ab0146056aadb6b0a81e2a602
Cormac Mattimoe <cormacmattimoe98@gmail.com>
2020-06-19 14:50:02 +0100

    updated list

--- src/app/services/mock-data.ts
+++ src/app/services/mock-data.ts
@@ -1,100 +1,503 @@
 export const USERS : any[] =
 
-[
-    {
-        name: "Paul Lennon",
-        accountNumber: "234567",
-        country:" England ",
-        dateJoined : "11 feb 2011"
-    },
-    {
-        name: "Nigel Crawfish",
-        accountNumber: "345678",
-        country:" Ireland ",
-        dateJoined : "23 feb 1999"
-    },
-    {
-        name: "Niki Ferguson",
-        accountNumber: "345678",
-        country:" Spain",
-        dateJoined : "08 feb 1878"
-    },
-    {
-        name: "Paul Lynch",
-        accountNumber: "987654",
-        country:" Iceland",
-        dateJoined : "27 feb 1987"
-    },
-    {
-        name: "Paul Lennon",
-        accountNumber: "234567",
-        country:" England ",
-        dateJoined : "11 feb 2011"
-    },
-    {
-        name: "Nigel Crawfish",
-        accountNumber: "345678",
-        country:" Ireland ",
-        dateJoined : "23 feb 1999"
-    },
-    {
-        name: "Niki Ferguson",
-        accountNumber: "345678",
-        country:" Spain",
-        dateJoined : "08 feb 1878"
-    },
-    {
-        name: "Paul Lynch",
-        accountNumber: "987654",
-        country:" Iceland",
-        dateJoined : "27 feb 1987"
-    },
-    {
-        name: "Paul Lennon",
-        accountNumber: "234567",
-        country:" England ",
-        dateJoined : "11 feb 2011"
-    },
-    {
-        name: "Nigel Crawfish",
-        accountNumber: "345678",
-        country:" Ireland ",
-        dateJoined : "23 feb 1999"
-    },
-    {
-        name: "Niki Ferguson",
-        accountNumber: "345678",
-        country:" Spain",
-        dateJoined : "08 feb 1878"
-    },
-    {
-        name: "Paul Lynch",
-        accountNumber: "987654",
-        country:" Iceland",
-        dateJoined : "27 feb 1987"
-    },
-    {
-        name: "Paul Lennon",
-        accountNumber: "234567",
-        country:" England ",
-        dateJoined : "11 feb 2011"
-    },
-    {
-        name: "Nigel Crawfish",
-        accountNumber: "345678",
-        country:" Ireland ",
-        dateJoined : "23 feb 1999"
-    },
-    {
-        name: "Niki Ferguson",
-        accountNumber: "345678",
-        country:" Spain",
-        dateJoined : "08 feb 1878"
-    },
-    {
-        name: "Paul Lynch",
-        accountNumber: "987654",
-        country:" Iceland",
-        dateJoined : "27 feb 1987"
-    },
-    ]
+[{
+    "name": "Lee Heyball",
+    "accountNumber": "86",
+    "country": "China",
+    "dateJoined": "6/1/2016"
+  }, {
+    "name": "Janna Szach",
+    "accountNumber": "90",
+    "country": "Cameroon",
+    "dateJoined": "12/2/2007"
+  }, {
+    "name": "Urbanus Wadhams",
+    "accountNumber": "2",
+    "country": "Brazil",
+    "dateJoined": "2/17/2018"
+  }, {
+    "name": "Padgett Gellately",
+    "accountNumber": "88",
+    "country": "Tanzania",
+    "dateJoined": "9/2/2004"
+  }, {
+    "name": "Leontine Mingaud",
+    "accountNumber": "42",
+    "country": "Japan",
+    "dateJoined": "10/21/2015"
+  }, {
+    "name": "Patty Fransson",
+    "accountNumber": "3",
+    "country": "Sri Lanka",
+    "dateJoined": "10/18/2008"
+  }, {
+    "name": "Samaria Rougier",
+    "accountNumber": 7,
+    "country": "Philippines",
+    "dateJoined": "5/2/2000"
+  }, {
+    "name": "Fallon Bracknall",
+    "accountNumber": 24,
+    "country": "Poland",
+    "dateJoined": "5/15/2018"
+  }, {
+    "name": "Giovanna Bayless",
+    "accountNumber": 29,
+    "country": "Brazil",
+    "dateJoined": "9/6/2015"
+  }, {
+    "name": "Hana Cordell",
+    "accountNumber": 91,
+    "country": "South Africa",
+    "dateJoined": "5/10/2000"
+  }, {
+    "name": "Carlina Cashmore",
+    "accountNumber": 100,
+    "country": "Brazil",
+    "dateJoined": "5/21/2010"
+  }, {
+    "name": "Blancha Ladel",
+    "accountNumber": 12,
+    "country": "Portugal",
+    "dateJoined": "11/2/2008"
+  }, {
+    "name": "Willi Labat",
+    "accountNumber": 27,
+    "country": "Indonesia",
+    "dateJoined": "6/6/2004"
+  }, {
+    "name": "Wait Wilkin",
+    "accountNumber": 48,
+    "country": "Brazil",
+    "dateJoined": "10/1/2002"
+  }, {
+    "name": "Raymond Bolderson",
+    "accountNumber": 37,
+    "country": "Russia",
+    "dateJoined": "9/26/2002"
+  }, {
+    "name": "Llywellyn Dory",
+    "accountNumber": 55,
+    "country": "Indonesia",
+    "dateJoined": "2/10/2017"
+  }, {
+    "name": "Burk Sends",
+    "accountNumber": 5,
+    "country": "France",
+    "dateJoined": "4/7/2019"
+  }, {
+    "name": "Catherin Leeds",
+    "accountNumber": 88,
+    "country": "Kyrgyzstan",
+    "dateJoined": "8/23/2018"
+  }, {
+    "name": "Moritz Ferens",
+    "accountNumber": 96,
+    "country": "Indonesia",
+    "dateJoined": "8/9/2009"
+  }, {
+    "name": "Ilsa Millson",
+    "accountNumber": 89,
+    "country": "Czech Republic",
+    "dateJoined": "3/6/2019"
+  }, {
+    "name": "Nancy Spendlove",
+    "accountNumber": 64,
+    "country": "New Zealand",
+    "dateJoined": "5/3/2014"
+  }, {
+    "name": "Winfield Greenmon",
+    "accountNumber": 90,
+    "country": "Indonesia",
+    "dateJoined": "11/12/2013"
+  }, {
+    "name": "Allen Dalglish",
+    "accountNumber": 78,
+    "country": "Indonesia",
+    "dateJoined": "2/17/2019"
+  }, {
+    "name": "Michal Antoshin",
+    "accountNumber": 27,
+    "country": "Poland",
+    "dateJoined": "9/25/2003"
+  }, {
+    "name": "Ange Goodwins",
+    "accountNumber": 5,
+    "country": "Australia",
+    "dateJoined": "4/2/2006"
+  }, {
+    "name": "Lorrie Skates",
+    "accountNumber": 58,
+    "country": "China",
+    "dateJoined": "12/14/2012"
+  }, {
+    "name": "Grover Feltham",
+    "accountNumber": 40,
+    "country": "Angola",
+    "dateJoined": "1/4/2019"
+  }, {
+    "name": "Jobey Train",
+    "accountNumber": 94,
+    "country": "Canada",
+    "dateJoined": "5/4/2004"
+  }, {
+    "name": "Nickey Gebbie",
+    "accountNumber": 95,
+    "country": "Philippines",
+    "dateJoined": "1/11/2015"
+  }, {
+    "name": "Deanne Northill",
+    "accountNumber": 4,
+    "country": "Brazil",
+    "dateJoined": "4/5/2001"
+  }, {
+    "name": "Sean O'Caherny",
+    "accountNumber": 53,
+    "country": "Philippines",
+    "dateJoined": "1/10/2006"
+  }, {
+    "name": "Ernst Greensite",
+    "accountNumber": 78,
+    "country": "Brazil",
+    "dateJoined": "5/3/2003"
+  }, {
+    "name": "Laurice Leheude",
+    "accountNumber": 79,
+    "country": "Bangladesh",
+    "dateJoined": "5/1/2015"
+  }, {
+    "name": "Burgess O'Cuddie",
+    "accountNumber": 4,
+    "country": "Finland",
+    "dateJoined": "5/10/2010"
+  }, {
+    "name": "Reggy Saket",
+    "accountNumber": 6,
+    "country": "China",
+    "dateJoined": "5/31/2010"
+  }, {
+    "name": "Malissia Hinckesman",
+    "accountNumber": 74,
+    "country": "Brazil",
+    "dateJoined": "10/27/2019"
+  }, {
+    "name": "Hyacinthie Alfwy",
+    "accountNumber": 96,
+    "country": "Brazil",
+    "dateJoined": "1/2/2012"
+  }, {
+    "name": "Rudd Sutherden",
+    "accountNumber": 3,
+    "country": "Armenia",
+    "dateJoined": "9/30/2006"
+  }, {
+    "name": "Ernst Challin",
+    "accountNumber": 80,
+    "country": "United States",
+    "dateJoined": "10/22/2016"
+  }, {
+    "name": "Nicki Campion",
+    "accountNumber": 53,
+    "country": "China",
+    "dateJoined": "5/30/2018"
+  }, {
+    "name": "Regen Hartegan",
+    "accountNumber": 55,
+    "country": "Japan",
+    "dateJoined": "8/22/2001"
+  }, {
+    "name": "Tansy Dillow",
+    "accountNumber": 33,
+    "country": "Japan",
+    "dateJoined": "9/4/2015"
+  }, {
+    "name": "Lucine Culleford",
+    "accountNumber": 14,
+    "country": "Russia",
+    "dateJoined": "12/19/2018"
+  }, {
+    "name": "Ham Strevens",
+    "accountNumber": 21,
+    "country": "Indonesia",
+    "dateJoined": "7/21/2007"
+  }, {
+    "name": "Valentin Maasz",
+    "accountNumber": 71,
+    "country": "Vietnam",
+    "dateJoined": "11/24/2004"
+  }, {
+    "name": "Joice Guiver",
+    "accountNumber": 82,
+    "country": "Thailand",
+    "dateJoined": "1/13/2005"
+  }, {
+    "name": "Adda Soda",
+    "accountNumber": 53,
+    "country": "Japan",
+    "dateJoined": "4/13/2012"
+  }, {
+    "name": "Juliann Coast",
+    "accountNumber": 8,
+    "country": "Iceland",
+    "dateJoined": "9/1/2003"
+  }, {
+    "name": "Blaire Dradey",
+    "accountNumber": 25,
+    "country": "Jordan",
+    "dateJoined": "5/21/2014"
+  }, {
+    "name": "Conny Dahlberg",
+    "accountNumber": 71,
+    "country": "Sweden",
+    "dateJoined": "6/14/2016"
+  }, {
+    "name": "Georgena Giacomuzzi",
+    "accountNumber": 42,
+    "country": "France",
+    "dateJoined": "9/1/2008"
+  }, {
+    "name": "Giustina Blenkensop",
+    "accountNumber": 22,
+    "country": "France",
+    "dateJoined": "8/26/2013"
+  }, {
+    "name": "Pooh Brunn",
+    "accountNumber": 29,
+    "country": "Indonesia",
+    "dateJoined": "3/5/2011"
+  }, {
+    "name": "Killian Portt",
+    "accountNumber": 33,
+    "country": "Papua New Guinea",
+    "dateJoined": "9/3/2018"
+  }, {
+    "name": "Janeen Featherstonhalgh",
+    "accountNumber": 100,
+    "country": "Venezuela",
+    "dateJoined": "11/29/2014"
+  }, {
+    "name": "Wendall Tiddeman",
+    "accountNumber": 65,
+    "country": "Russia",
+    "dateJoined": "5/21/2007"
+  }, {
+    "name": "Teador Astlet",
+    "accountNumber": 30,
+    "country": "Egypt",
+    "dateJoined": "6/12/2003"
+  }, {
+    "name": "Ruthanne Bardsley",
+    "accountNumber": 79,
+    "country": "Philippines",
+    "dateJoined": "7/17/2004"
+  }, {
+    "name": "Hakim Penniall",
+    "accountNumber": 30,
+    "country": "China",
+    "dateJoined": "3/3/2007"
+  }, {
+    "name": "Ogden Ruffler",
+    "accountNumber": 70,
+    "country": "Russia",
+    "dateJoined": "5/25/2019"
+  }, {
+    "name": "Benson Matzl",
+    "accountNumber": 14,
+    "country": "Ireland",
+    "dateJoined": "4/21/2010"
+  }, {
+    "name": "Georgia Found",
+    "accountNumber": 83,
+    "country": "France",
+    "dateJoined": "9/1/2004"
+  }, {
+    "name": "Ben Samudio",
+    "accountNumber": 56,
+    "country": "France",
+    "dateJoined": "8/24/2004"
+  }, {
+    "name": "Calypso Spaughton",
+    "accountNumber": 16,
+    "country": "Greece",
+    "dateJoined": "1/25/2010"
+  }, {
+    "name": "Kittie Baribal",
+    "accountNumber": 66,
+    "country": "Poland",
+    "dateJoined": "9/5/2005"
+  }, {
+    "name": "Anna Lelande",
+    "accountNumber": 58,
+    "country": "Russia",
+    "dateJoined": "5/9/2007"
+  }, {
+    "name": "Valry Crallan",
+    "accountNumber": 80,
+    "country": "Czech Republic",
+    "dateJoined": "6/8/2017"
+  }, {
+    "name": "Deina Setterington",
+    "accountNumber": 30,
+    "country": "Sudan",
+    "dateJoined": "7/8/2004"
+  }, {
+    "name": "Opalina Padillo",
+    "accountNumber": 48,
+    "country": "China",
+    "dateJoined": "3/27/2000"
+  }, {
+    "name": "Nisse Lambden",
+    "accountNumber": 5,
+    "country": "Indonesia",
+    "dateJoined": "12/25/2018"
+  }, {
+    "name": "Ambros DelaField",
+    "accountNumber": 21,
+    "country": "Afghanistan",
+    "dateJoined": "2/24/2007"
+  }, {
+    "name": "Sarette Reynard",
+    "accountNumber": 9,
+    "country": "Japan",
+    "dateJoined": "10/22/2013"
+  }, {
+    "name": "Lizzie Lascell",
+    "accountNumber": 83,
+    "country": "Jamaica",
+    "dateJoined": "6/18/2006"
+  }, {
+    "name": "Lyda Orgee",
+    "accountNumber": 6,
+    "country": "Costa Rica",
+    "dateJoined": "1/12/2009"
+  }, {
+    "name": "Natty Yegorchenkov",
+    "accountNumber": 37,
+    "country": "Indonesia",
+    "dateJoined": "7/27/2011"
+  }, {
+    "name": "Joelly Fowley",
+    "accountNumber": 6,
+    "country": "China",
+    "dateJoined": "6/28/2019"
+  }, {
+    "name": "Loella Blissitt",
+    "accountNumber": 67,
+    "country": "China",
+    "dateJoined": "7/25/2019"
+  }, {
+    "name": "Liz Wimlet",
+    "accountNumber": 66,
+    "country": "Russia",
+    "dateJoined": "10/21/2004"
+  }, {
+    "name": "Huntington Stuckford",
+    "accountNumber": 15,
+    "country": "Belarus",
+    "dateJoined": "5/18/2010"
+  }, {
+    "name": "Tabina Ranyell",
+    "accountNumber": 69,
+    "country": "Russia",
+    "dateJoined": "10/6/2016"
+  }, {
+    "name": "Thalia McPheat",
+    "accountNumber": 23,
+    "country": "Poland",
+    "dateJoined": "6/25/2008"
+  }, {
+    "name": "Inglebert Brounfield",
+    "accountNumber": 85,
+    "country": "South Korea",
+    "dateJoined": "3/17/2001"
+  }, {
+    "name": "Norman Deners",
+    "accountNumber": 18,
+    "country": "China",
+    "dateJoined": "8/18/2018"
+  }, {
+    "name": "Cazzie Keningley",
+    "accountNumber": 78,
+    "country": "Syria",
+    "dateJoined": "8/12/2004"
+  }, {
+    "name": "Diahann Skewis",
+    "accountNumber": 97,
+    "country": "Thailand",
+    "dateJoined": "10/4/2003"
+  }, {
+    "name": "Porter Snipe",
+    "accountNumber": 94,
+    "country": "Russia",
+    "dateJoined": "9/14/2008"
+  }, {
+    "name": "Simonette Crumby",
+    "accountNumber": 55,
+    "country": "China",
+    "dateJoined": "8/27/2011"
+  }, {
+    "name": "Andreana Toffoletto",
+    "accountNumber": 68,
+    "country": "China",
+    "dateJoined": "6/23/2001"
+  }, {
+    "name": "Abramo Braunlein",
+    "accountNumber": 56,
+    "country": "Thailand",
+    "dateJoined": "2/2/2010"
+  }, {
+    "name": "Alleyn Postance",
+    "accountNumber": 79,
+    "country": "China",
+    "dateJoined": "12/18/2004"
+  }, {
+    "name": "Seka Sandcroft",
+    "accountNumber": 45,
+    "country": "Philippines",
+    "dateJoined": "1/21/2006"
+  }, {
+    "name": "Cristina McCarle",
+    "accountNumber": 19,
+    "country": "Tanzania",
+    "dateJoined": "12/16/2004"
+  }, {
+    "name": "Arlette McCluskey",
+    "accountNumber": 35,
+    "country": "Canada",
+    "dateJoined": "6/26/2019"
+  }, {
+    "name": "Fredrika Boustred",
+    "accountNumber": 8,
+    "country": "Mexico",
+    "dateJoined": "7/14/2009"
+  }, {
+    "name": "Zea Kerford",
+    "accountNumber": 90,
+    "country": "Philippines",
+    "dateJoined": "7/29/2015"
+  }, {
+    "name": "Leanna Murley",
+    "accountNumber": 69,
+    "country": "Sweden",
+    "dateJoined": "7/24/2011"
+  }, {
+    "name": "Joya Ramme",
+    "accountNumber": 27,
+    "country": "Indonesia",
+    "dateJoined": "12/11/2006"
+  }, {
+    "name": "Guinna Pitt",
+    "accountNumber": 21,
+    "country": "Libya",
+    "dateJoined": "10/25/2005"
+  }, {
+    "name": "Kalie Bousquet",
+    "accountNumber": 56,
+    "country": "Brazil",
+    "dateJoined": "11/14/2000"
+  }, {
+    "name": "Lewes Kinker",
+    "accountNumber": 91,
+    "country": "Bosnia and Herzegovina",
+    "dateJoined": "3/26/2014"
+  }]
