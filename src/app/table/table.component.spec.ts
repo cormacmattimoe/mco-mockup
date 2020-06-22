7288092cde528a9d6992853c00f085ee9315043f
Cormac Mattimoe <cormacmattimoe98@gmail.com>
2020-06-22 16:15:33 +0100

    table updates

--- src/app/table/table.component.spec.ts
+++ src/app/table/table.component.spec.ts
@@ -19,7 +19,7 @@ describe('TableComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
